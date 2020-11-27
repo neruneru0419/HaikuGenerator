438f54e0b39ad5ae02cef4939aac4fa25d747932
neruneru0419 <neruneru55555@gmail.com>
2020-11-27 22:00:03 +0900

    俳句の生成の処理

--- 575.rb
+++ 575.rb
@@ -1,76 +1,101 @@
-require "twitter"
-require "natto"
-require "net/http"
+require 'twitter'
+require 'natto'
 
-
-#{文節, 文節の長さ, 品詞}
 class NattoParser
-  def initialize()
-    @nm = Natto::MeCab.new()
-  end
-
-  def get_hiragana_sentence(sentence)
-    uri = URI.parse("https://labs.goo.ne.jp/api/hiragana")
-    params = {
-        app_id: ENV["goo_apikey"],
-        sentence: sentence,
-        output_type: "hiragana"
-    }
-    response = Net::HTTP.post_form(uri, params)
-    return JSON.parse(response.body)
+  def initialize
+    @nm = Natto::MeCab.new
   end
 
   def parse(timeline_tweet)
-    clause_list = Array.new
-    kana_tweet = String.new
+    clause_list = []
+    kana_tweet = ''
     cnt = 0
     @nm.parse(timeline_tweet) do |n|
-        if n.surface != "" then
-            clause_list.push({"clause": n.surface}) #文節を格納
-            if n.surface.match?(/[一-龠々]/) then #漢字の検知
-                kana_tweet = n.feature.gsub(/.*,/, "") #漢字の読みを抽出
-            else
-                kana_tweet = n.surface
-            end
-            hiragana_tweet = get_hiragana_sentence(kana_tweet)
-            clause_list[cnt][:clause_length] = get_number_of_notes(hiragana_tweet["converted"]) #文節の長さを格納
-            clause_list[cnt][:part_of_speech] = n.feature.match(/.{1,2}詞/)[0]
-            cnt += 1
-        end
+      if n.surface != '' && n.feature[0..1] != '記号'
+        clause_list.push({ "clause": n.surface }) # 文節を格納
+        kana_tweet = if n.surface.match?(/[一-龠々]/) # 漢字の検知
+                       n.feature.gsub(/.*,/, '') # 漢字の読みを抽出
+                     else
+                       n.surface
+                     end
+        hiragana_tweet = kana_tweet.tr('ァ-ン', 'ぁ-ん')
+        clause_list[cnt][:clause_length] = get_number_of_notes(hiragana_tweet) # 文節の長さを格納
+        clause_list[cnt][:part_of_speech] = n.feature.match(/.{1,2}詞/)[0]
+        cnt += 1
+      end
     end
-    p clause_list
-    return clause_list[0..-2] #EOSのブロックを除去
+    clause_list # EOSのブロックを除去
   end
 end
+
 def get_number_of_notes(sentence)
-  abandoned_kana = ["ァ", "ィ", "ゥ", "ェ", "ォ", "ャ", "ュ", "ョ"] #捨て仮名を格納
-  abandoned_kana.each{ |kana| sentence.delete!(kana)}
-  return sentence.length
+  abandoned_kana = %w[ァ ィ ゥ ェ ォ ャ ュ ョ] # 捨て仮名を格納
+  abandoned_kana.each { |kana| sentence.delete!(kana) }
+  sentence.length
 end
-def chain_notes(length_list)
+
+def chain_notes(sentence_block)
   sum = 0
   five_sentence = []
-  length_list.each do |length|
-    sum += length
-    if 5 <= sum then
-      five_sentence.push(sum)
-      sum = 0
+  seven_sentence = []
+  sentence = ''
+  start_part_of_speech = ''
+  end_part_of_speech = ''
+
+  while sentence_block.empty? == false
+    sentence_block.each do |clause_information|
+      sum += clause_information[:clause_length]
+      sentence += clause_information[:clause]
+      start_part_of_speech = clause_information[:part_of_speech] if start_part_of_speech == ''
+      next unless (5 == sum) || (7 == sum)
+
+      # print sentence, sum, clause_information[:part_of_speech]
+      # puts ''
+      if sum == 7
+        end_part_of_speech = clause_information[:part_of_speech]
+        print sentence + " " + start_part_of_speech + " " + end_part_of_speech
+        puts ""
+        seven_sentence.push(sentence) if start_part_of_speech == '名詞' && (end_part_of_speech == '名詞' || end_part_of_speech == '助詞')
+        start_part_of_speech = ''
+
+      elsif sum == 5
+        end_part_of_speech = clause_information[:part_of_speech]
+        five_sentence.push(sentence) if start_part_of_speech == '名詞' && (end_part_of_speech == '名詞' || end_part_of_speech == '助詞')
+      end
     end
+    sentence_block.delete_at(0)
+    sentence = ''
+    sum = 0
+    start_part_of_speech = ''
   end
+  [five_sentence, seven_sentence]
 end
-natto_parser = NattoParser.new()
-kana_block = natto_parser.parse("仕事終わってギター弾いてたらこんな時間")
-#kana_length = number_of_notes(kana_block)
+
+
+def genarate_haiku(notes_length)
+  puts notes_length[0].sample
+  puts notes_length[1].sample
+  puts notes_length[0].sample
+end
+natto_parser = NattoParser.new
+sentence_block = natto_parser.parse("三つの単語で名前つくるやつ、パスを送るのが楽しい")
+notes_length = chain_notes(sentence_block)
+p notes_length
+genarate_haiku(notes_length) unless notes_length[0].empty? && notes_length[1].empty?
 =begin
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key    = ENV
-  config.consumer_secret = ENV
-  config.access_token    = ENV
-  config.access_token_secret = ENV
+  config.consumer_key    = ENV['MY_CONSUMER_KEY']
+  config.consumer_secret = ENV['MY_CONSUMER_SECRET']
+  config.access_token    = ENV['MY_ACCESS_TOKEN']
+  config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
 end
-timeline_tweet = []
-client.home_timeline({count: 100}).each do |tweet|
-    timeline_tweet.push(tweet.text)    
+client.home_timeline({ count: 100, exclude_replies: true, include_rts: false, include_entities: false }).each do |tweet|
+  next if tweet.text.include?('http')
+
+  puts tweet.text
+  sentence_block = natto_parser.parse(tweet.text)
+  notes_length = chain_notes(sentence_block)
+  p notes_length
+  genarate_haiku(notes_length) unless notes_length[0].empty? && notes_length[1].empty?
 end
-p timeline_tweet
 =end
